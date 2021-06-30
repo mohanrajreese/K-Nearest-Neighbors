9d1d7041cfe7e5c6ae34d1ac30426739162ed6f5
mohanrajreese <mohanrj1345@gmail.com>
2021-07-01 02:47:24 +0530

    For DMA presentation

--- knn.py
+++ knn.py
@@ -1,12 +1,11 @@
 import pandas as pd                                      # For importing data from neraya formats like CSV, JSON, SQL
 from sklearn.model_selection import train_test_split     # Split arrays or matrices into random train and test subsets
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.metrics import confusion_matrix             # Compute confusion matrix to evaluate the accuracy 
-                                                         #                              of a classification.
+from sklearn.metrics import confusion_matrix            # Compute confusion matrix to evaluate the accuracy of classification
 df = pd.read_csv('heart.csv')
 
 y = df['output']
-X = df.drop('output', axis = 1)
+X = df.drop('output', axis = 1)                     
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.5, random_state = 0)
 model = KNeighborsClassifier(n_neighbors=6)
 model.fit(X_train,y_train)
